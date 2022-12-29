e516fbd5573ac18e34309a02583de6628003fa64
Snare <sh0n556@bk.ru>
2022-12-29 15:18:28 +0300

    slight changes in logic

--- Server/main.cpp
+++ Server/main.cpp
@@ -17,12 +17,20 @@ int main()
 	if (pipe_manager.last_error())
 		return 1;
 
-	pipe_manager.send_message("file.txt");
+	std::string file_name;
+	std::cin >> file_name;
+
+	pipe_manager.send_message(file_name);
 
 	if (pipe_manager.last_error())
 		return 1;
 
-	pipe_manager.send_message("-");
+	char symbol_to_insert = 0;
+	std::cin >> symbol_to_insert;
+	std::string sti_str;
+	sti_str += symbol_to_insert;
+
+	pipe_manager.send_message(sti_str);
 
 	if (pipe_manager.last_error())
 		return 1;
